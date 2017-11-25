a4660e5502e966eedbf7f09afb64e579ddaf6321
f-istvan <f.istvan1984@gmail.com>
2017-11-25 21:58:42 +0100

    new improvements:
    - add iteration delay time
    - add Thread.sleep
    - add Game type
    - implement iteration over living cells

--- src/game_of_life/run.clj
+++ src/game_of_life/run.clj
@@ -7,22 +7,61 @@
 ;; 3) Any live cell with more than three live neighbours dies, as if by overcrowding.
 ;; 4) Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
 
-;; useful functions
-;; assoc-in
-;; get-in
+(defrecord Game [iteration-delay-in-millis
+                 map-size
+                 living-cells]
 
-(defn start-game
-  "Entry point"
-  []
-  (println "Starting Game Of Life...")
-  (let [data-file (io/resource "init-world.txt")]
-    (println (slurp data-file))))
+  Object
+  (toString [_]
+    (str "Iteration delay in millis: " iteration-delay-in-millis
+         " Map size: " map-size
+         " Living cells: " living-cells)))
+
+(defn gonna-live?
+  [cell]
+  true)
+
+(defn create-next-generation
+  [game]
+  (let [next-generation
+        (for [cell (:living-cells game)
+              :when (gonna-live? cell)]
+          cell)]
+
+    (Game.
+          (:iteration-delay-in-millis game)
+          (:map-size game)
+          next-generation)))
+
+(defn print-map
+  [game]
+  (println game))
+
+(defn run-game
+  [game]
+  (let [delay (:iteration-delay-in-millis game)]
+    (loop [game game]
+        (print-map game)
+        (Thread/sleep delay)
+        (recur (create-next-generation game)))))
 
 (defn test-fn
-  "A function for testing purpuses"
+  "TODO remove this fn. A function for testing purpuses."
   [x]
   x)
 
+(defn read-game-data-from-file
+  [filename]
+  (let [data-file (io/resource filename)
+        init-state (read-string (slurp data-file))
+        game (Game.
+              (:iteration-delay-in-millis init-state)
+              (:map-size init-state)
+              (:living-cells init-state))]
+    game))
+
 (defn -main
   [& args]
-  (start-game))
+  (let [game (read-game-data-from-file "init-world.txt")]
+    (println "Starting Game Of Life...")
+    (run-game game)))
